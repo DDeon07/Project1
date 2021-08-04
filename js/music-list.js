d38772db4706d250933e9be01fdc2d9022602026
DDeon07 <88461847+DDeon07@users.noreply.github.com>
2021-08-04 21:42:33 +0000

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,63 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+var fav;
+let allMusic = [
+  {
+    name: "Rhythms of Grace",
+    artist: "Hillsong United",
+    img: "music-1",
+    src: "music-1",
+    fav: "false"
+
+  },
+  {
+    name: "Bones",
+    artist: "Hillsong United",
+    img: "music-2",
+    src: "music-2",
+    fav: "false"
+  },
+  {
+    name: "Light Will Shine",
+    artist: "Hillsong United",
+    img: "music-3",
+    src: "music-3",
+    fav: "false"
+  },
+  {
+    name: "Street Called Mercy",
+    artist: "All United",
+    img: "music-4",
+    src: "music-4",
+    fav: "false"
+  },
+  {
+    name: "Prince of Peace",
+    artist: "All United",
+    img: "music-5",
+    src: "music-5",
+    fav: "false"
+  },
+  {
+    name: "Closer Than You Know",
+    artist: "Hillsong United",
+    img: "music-6",
+    src: "music-6",
+    fav: "false"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember image of the music must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember music must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
